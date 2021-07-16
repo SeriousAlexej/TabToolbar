2dbe41492646a0cb93b9bc64c0feacbc3c31414b
liuzhao <liuzhao071@126.com>
2021-07-16 11:25:35 +0800

    add example SaveQss

---
+++ examples/SaveQss/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <QApplication>
+#include "TabToolbar/StyleTools.h"
+#include "TabToolbar/Styles.h"
+#include <QFile>
+
+using namespace tt;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+	QStringList styleNames = GetRegisteredStyles();
+	std::unique_ptr<StyleParams> pStyleParams;
+    QString appDirPath = QApplication::applicationDirPath();
+	for (const QString & styleName : styleNames)
+	{
+		pStyleParams.reset(CreateStyle(styleName).release());
+		QString styleSheet = GetSheetForStyle(*pStyleParams);
+
+		QFile qssFile(appDirPath + "/" + styleName + ".qss");
+		qssFile.open(QFile::WriteOnly);
+		qssFile.write(styleSheet.toLocal8Bit());
+		qssFile.close();
+	}
+
+//    int ret=a.exec();
+    return 0;
+}
