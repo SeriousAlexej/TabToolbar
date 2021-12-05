961e8d21a15edc1473f84469aa2fae480ed978d5
Juraj Oršulić <juraj.orsulic@fer.hr>
2021-12-05 22:56:26 +0100

    Update ToolButtonStyle.cpp

--- src/TabToolbar/ToolButtonStyle.cpp
+++ src/TabToolbar/ToolButtonStyle.cpp
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with TabToolbar.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <QApplication>
 #include <QPainter>
 #include <QStyleOptionToolButton>
 #include "ToolButtonStyle.h"
